0362c4a968b99094433a038f746ebbec840d5116
Gunawan <gunawan.firdaus99@ymail.com>
2024-02-16 22:25:54 +0700

    update penambahan relay

--- rfid.py
+++ rfid.py
@@ -2,8 +2,18 @@ from time import sleep
 from os import system
 from serial import *
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+relay1 = 17
+relay2 = 18
+
+GPIO.setup(relay1, GPIO.OUT)
+GPIO.setup(relay2, GPIO.OUT)
 
 test_serial = Serial('COM3', 115200, timeout=0.1)
+test_serial2 = Serial('COM4', 115200, timeout=0.1)
 
 INVENTORY = 'BB 00 22 00 00 22 7E' #Single Inventory
 
@@ -36,6 +46,16 @@ def send_cmd(cmd):
     
     return hex_space
 
+def send_cmd2(cmd):
+    data = bytes.fromhex(cmd)
+    test_serial2.write(data)
+    response = test_serial2.read(512)
+    response_hex = response.hex().upper()
+    hex_list = [response_hex[i:i+2] for i in range(0, len(response_hex), 2)]
+    hex_space = ' '.join(hex_list)
+    
+    return hex_space
+
 def connect_to_broker(broker_address, port, username, password):
     broker_address = "127.0.0.1"
     port = 1883
@@ -51,13 +71,36 @@ def publish_message(client, topic, message):
     client.publish(topic, message)
     print(f"Published message: {message}")
 
+def turn_on():
+    GPIO.output(relay1, GPIO.HIGH)
+
+def turn_on2():
+    GPIO.output(relay2, GPIO.HIGH)
+    
+def turn_off():
+    GPIO.output(relay1, GPIO.LOW)
+
+def turn_off2():
+    GPIO.output(relay2, GPIO.LOW)
+
 def main():
     terdaftar = 'E8 79 30 00 E2 80 68 94 00 00 40 17 43 54 A1 14 00 00 00 00'
     read1 = send_cmd(readUserMemory1)
+    read2 = send_cmd2(readUserMemory1)
     sliced = read1[60:119]
+    sliced2 = read2[60:119]
     print(read1)
     if sliced == terdaftar:
+        turn_on()
         client.publish("rfid", "FL1")
+    else:
+        turn_off()
+    
+    if sliced2 == terdaftar:
+        turn_on2()
+        client.publish("rfid", "FL1")
+    else:
+        turn_off2()
 
 if __name__ == '__main__':
     
