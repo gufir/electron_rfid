d461ec08ad4212a65f77ffc39953aa8c842919e4
Gunawan Firdaus <82512652+gufir@users.noreply.github.com>
2024-02-18 16:21:40 +0700

    Update with Relay

--- rfid.py
+++ rfid.py
@@ -1,19 +1,45 @@
 from time import sleep
 from os import system
 from serial import *
-import paho.mqtt.client as mqtt
+import mysql.connector
+from datetime import datetime
 import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt_client
+
 
 GPIO.setmode(GPIO.BCM)
-
-relay1 = 17
-relay2 = 18
+GPIO.setwarnings(False)
+relay1 = 14
+relay2 = 15
 
 GPIO.setup(relay1, GPIO.OUT)
 GPIO.setup(relay2, GPIO.OUT)
 
-test_serial = Serial('COM3', 115200, timeout=0.1)
-test_serial2 = Serial('COM4', 115200, timeout=0.1)
+def on_relay(relay):
+    GPIO.output(relay, GPIO.HIGH)
+
+def off_relay(relay):
+    GPIO.output(relay, GPIO.LOW)
+
+def insert_db(status):
+
+    mydb = mysql.connector.connect(
+    host="raspberrypi.local",
+    user="pi",
+    password="raspberry",
+    database="Dojo"
+)
+    now = datetime.now()
+    now = now.strftime('%Y-%m-%d %H:%M:%S')
+    mycursor = mydb.cursor()
+    query = "INSERT INTO Conveyor (DATE_TIME,STATUS) VALUES (%s,%s)"
+    mycursor.execute(query,(now,status))
+    mydb.commit()
+
+    return print("Insert Success")
+
+test_serial = Serial('/dev/ttyUSB0', 115200, timeout=0.1)
+test_serial2 = Serial('/dev/ttyUSB1', 115200, timeout=0.1)
 
 INVENTORY = 'BB 00 22 00 00 22 7E' #Single Inventory
 
@@ -24,8 +50,6 @@ STOP = 'BB	00	28	00 00	28	7E' #Stop Multi Inventory
 READ = 'BB 00 39 00 09 00 00 00 00 03 00 00 00 02 45 7E' #Read User Memory
 
 VERSION = 'BB 00 03 00 01 00 04 7E'
-
-client = mqtt.Client()
 
 readUserMemory0 = 'BB 00 39 00 09 00 00 00 00 00 00 00 00 07 49 7E'
 # 0 Reserved Memory
@@ -56,57 +80,38 @@ def send_cmd2(cmd):
     
     return hex_space
 
-def connect_to_broker(broker_address, port, username, password):
-    broker_address = "127.0.0.1"
-    port = 1883
-    username = "pi"
-    password = "raspberry"
-
-    client = mqtt.Client()
-    client.username_pw_set(username, password)
-    client.connect(broker_address, port)
-    return client
-
-def publish_message(client, topic, message):
-    client.publish(topic, message)
-    print(f"Published message: {message}")
-
-def turn_on():
-    GPIO.output(relay1, GPIO.HIGH)
-
-def turn_on2():
-    GPIO.output(relay2, GPIO.HIGH)
-    
-def turn_off():
-    GPIO.output(relay1, GPIO.LOW)
-
-def turn_off2():
-    GPIO.output(relay2, GPIO.LOW)
-
 def main():
     terdaftar = 'E8 79 30 00 E2 80 68 94 00 00 40 17 43 54 A1 14 00 00 00 00'
+    terdaftar1 = '19 63 30 00 E2 80 68 94 00 00 50 17 43 54 A1 4F 00 00 00 00'
+    terdaftar2 = '19 63 30 00 E2 80 68 94 00 00 50 17 43 54 A1 4F 00 00 00 00'
     read1 = send_cmd(readUserMemory1)
-    read2 = send_cmd2(readUserMemory1)
     sliced = read1[60:119]
-    sliced2 = read2[60:119]
     print(read1)
-    if sliced == terdaftar:
-        turn_on()
-        client.publish("rfid", "FL1")
-    else:
-        turn_off()
-    
-    if sliced2 == terdaftar:
-        turn_on2()
-        client.publish("rfid", "FL1")
-    else:
-        turn_off2()
+    if sliced == terdaftar1:
+        status = "1"
+        insert_db(status)
+        on_relay(relay1)
+
+    read2 = send_cmd2(readUserMemory1)
+    sliced2 = read2[60:119]
+    print(read2)
+
+    if sliced2 == terdaftar1:
+        status = "0"
+        insert_db(status)
+        off_relay(relay1)
+
 
 if __name__ == '__main__':
-    
     while True:
         # read1 = send_cmd(readUserMemory1)
         # print(read1)
         main()
         sleep(0.5)
-        system('cls')
+        system('clear')
+
+# main()
+# off_relay(relay1)
+        
+# var = send_cmd(readUserMemory1)
+# print(var)
